6208f644e1fc22021b780f8ffb504d2c6a85b9db
Steven Mirabito <stevenmirabito@gmail.com>
2019-09-02 00:47:25 -0700

    Use rTorrent daemon mode, update config syntax

--- rootfs/usr/local/bin/run.sh
+++ rootfs/usr/local/bin/run.sh
@@ -9,7 +9,7 @@ rm -f /data/.session/rtorrent.lock
 chown -R $UID:$GID /data /home/torrent /tmp /usr/flood/dist /flood-db /etc/s6.d
 
 if [ ${RTORRENT_SOCK} = "false" ]; then
-    sed -i -e 's|^scgi_local.*$|scgi_port = 0.0.0.0:5000|' /home/torrent/.rtorrent.rc
+    sed -i -e 's|^network.scgi.open_local.*$|network.scgi.open_port = 0.0.0.0:'${RTORRENT_SCGI}'|' /home/torrent/.rtorrent.rc
 fi
 
 exec su-exec $UID:$GID /bin/s6-svscan /etc/s6.d
